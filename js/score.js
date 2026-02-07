781fcb9513a7af915c6729bc01111552a48a6925
bbbfour <151484401+bbbfour@users.noreply.github.com>
2026-02-07 16:05:19 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
+    if (rank > 70) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
+    if (rank > 120 && percent < 100) {
         return 0;
     }
 
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 300) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
